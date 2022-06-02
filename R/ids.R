14b71805af7ed4e53293e7e66262b99afa518c3b
Sophie Moore <sophie.b.moore00@gmail.com>
2022-06-02 15:42:16 -0700

    wrote and tested get_artist_id

---
+++ R/ids.R
@@ -0,0 +1,16 @@
+#' Converts artist string to spotify id
+#'
+#' @param artist artist's name (string)
+#'
+#' @import dplyr
+#' @import stringr
+#'
+#' @export
+get_artist_id <- function(artist){
+  artist_str <- tolower(artist)
+  id <- artist_id_data %>%
+    filter(artist == artist_str) %>%
+    pull(id)
+
+  return(id)
+}
