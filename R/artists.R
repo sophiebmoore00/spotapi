eb9ef3c2fefd965402b045ca9dd897ae1f4d743e
Sophie Moore <sophie.b.moore00@gmail.com>
2022-06-05 15:32:55 -0700

    wrote top_songs function

--- R/artists.R
+++ R/artists.R
@@ -25,3 +25,30 @@ related_artists <- function(artist) {
   return(results)
 
 }
+
+#' Returns an artist's top songs
+#'
+#' @param artist artist's name
+#'
+#' @import glue
+#' @import jsonlite
+#' @import httr
+#' @import spotifyr
+#'
+#' @export
+top_songs <- function(artist) {
+  id <- get_artist_id(artist)
+  url <- glue("https://api.spotify.com/v1/artists/{id}/top-tracks")
+
+  prep_sys()
+  token <- get_spotify_access_token()
+
+  res <- GET(url, query = list(access_token = token, market = 'US'))
+  dat <- fromJSON(rawToChar(res$content))
+
+  results <- as.data.frame(dat$tracks$name)
+  colnames(results) <- "Song"
+
+  return(results)
+
+}
